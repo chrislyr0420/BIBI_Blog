4ac1f8f49f52cd2c177077c27211e1813e1db42e
chrislyr0420 <li.yiran@wustl.edu>
2020-09-13 23:19:41 -0700

    npm i xss to avoid xss attack

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -1,11 +1,12 @@
+const xss = require('xss')
 const { exec } = require('../db/mysql')
 
 const getList = (author, keyword) => {
     let sql = `select * from blogs where 1=1 `
-    if (author) {
+    if (xss(author)) {
         sql += `and author='${author}'`
     }
-    if (keyword) {
+    if (xss(keyword)) {
         sql += `and title like '%${keyword}%' `
     }
     sql += `order by createtime desc;`
@@ -15,7 +16,7 @@ const getList = (author, keyword) => {
 
 const getDetail = (id) => {
     let sql = `select * from blogs where 1=1 `
-    if (id) {
+    if (xss(id)) {
         sql += `and id='${id}'`
     }
     sql += `order by createtime desc;`
@@ -26,9 +27,9 @@ const getDetail = (id) => {
 
 const newBlog = (blogData = {}) => {
     if (blogData) {
-        const title = blogData.title
-        const content = blogData.content
-        const author = blogData.author
+        const title = xss(blogData.title)
+        const content = xss(blogData.content)
+        const author = xss(blogData.author)
 
         if (!title || !content || !author) {
             console.log("blogData is not enough")
@@ -47,8 +48,8 @@ const newBlog = (blogData = {}) => {
 }
 
 const updateBlog = (id, blogData = {}) => {
-    const title = blogData.title
-    const content = blogData.content
+    const title = xss(blogData.title)
+    const content = xss(blogData.content)
 
     let sql = `
         update blogs set title='${title}', content='${content}' where id='${id}';
@@ -65,7 +66,8 @@ const updateBlog = (id, blogData = {}) => {
 }
 
 const deleteBlog = (id, author) => {
-    let sql = `delete from blogs where id='${id}' and author='${author}'`
+
+    let sql = `delete from blogs where id='${xss(id)}' and author='${xss(author)}'`
     return exec(sql).then(deleteData => {
         if (deleteData.affectedRows > 0) {
             return true
