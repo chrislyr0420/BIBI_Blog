db973bb82a30e973fef8cd45009fbe2a539c2db9
chrislyr0420 <li.yiran@wustl.edu>
2020-09-14 04:17:43 -0700

    npm i express-session
    And enable session in app.js

--- blog-express/app.js
+++ blog-express/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -20,6 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'WJiol_8776',
+  cookie: {
+    path: '/', // this is by default
+    httpOnly: true, // this is also by default
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const { 
+    getList, 
+    getDetail, 
+    newBlog, 
+    updateBlog, 
+    deleteBlog } = require('../controller/blog')
 
 router.get('/list', function(req, res, next) {
-    res.json({
-        errno: 0,
-        data: [1, 2, 3]
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+    
+    // if (req.query.isadmin) {
+    //     const loginCheckResult = loginCheck(req)
+    //     if (loginCheckResult) {
+    //         return loginCheckResult
+    //     }
+
+    //     author = req.session.username
+    // }
+
+    const result = getList(author, keyword)
+    result.then(listData => {
+        res.json(new SuccessModel(listData))
     })
 })
 
