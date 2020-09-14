4aaaa2eb40eaf86d649770f50d5ee40b30d93e32
chrislyr0420 <li.yiran@wustl.edu>
2020-09-14 04:32:12 -0700

    Combine session with login check
    To test session, enable nginx reverse proxy, change port number from 3000 to 8000

--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const { userLogin } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-router.post('/login', function(req, res, next) {
+router.post('/login', (req, res, next) => {
     const { username, password } = req.body
 
-    res.json({
-        errno: 0,
-        data: {
-            username,
-            password
+    const result = userLogin(username, password)
+    result.then(data => {
+        if (data.username) {
+            req.session.username = data.username
+            req.session.realname = data.realname
+            // do not need to sync with redis here since express has it's own way to sync with redis
+            // set(req.sessionId, req.session)
+            res.json(new SuccessModel())
+        } else {
+            res.json(new ErrorModel('Login failed'))
         }
     })
+})
+
+router.get('/login-test', (req, res, next) => {
+    if (req.session.username) {
+        res.json({
+            error: 0,
+            msg: 'already login'
+        })
+    } else {
+        res.json({
+            errno: -1,
+            msg: 'not login'
+        })
+    }
 })
 
 module.exports = router;
